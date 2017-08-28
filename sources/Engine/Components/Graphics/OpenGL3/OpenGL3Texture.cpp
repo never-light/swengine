447849e6842d4d38f6d8259eaccd39c99d90f0d7
Nikolai Paukov <nic.paukov@yandex.ru>
2017-08-28 19:15:39 +0300

    Fixed textures flickering

--- sources/Engine/Components/Graphics/OpenGL3/OpenGL3Texture.cpp
+++ sources/Engine/Components/Graphics/OpenGL3/OpenGL3Texture.cpp
@@ -24,7 +24,7 @@ void OpenGL3Texture::loadFromFile(const std::string& filename) {
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	int nrChannels;
