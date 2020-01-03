c87d74a9411358250a0e191d6a10737c7ef13bf7
Nikolai Paukov <nic.paukov@yandex.ru>
2020-01-03 15:35:41 +0300

    Fix compilation warning in stb_image.h

--- ThirdParty/STB/stb_image.h
+++ ThirdParty/STB/stb_image.h
@@ -5238,6 +5238,7 @@ static int stbi__tga_get_comp(int bits_per_pixel, int is_grey, int* is_rgb16)
       case 8:  return STBI_grey;
       case 16: if(is_grey) return STBI_grey_alpha;
             // else: fall-through
+            [[fallthrough]];
       case 15: if(is_rgb16) *is_rgb16 = 1;
             return STBI_rgb;
       case 24: // fall-through
